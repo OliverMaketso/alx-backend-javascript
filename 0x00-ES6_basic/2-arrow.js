9b1932c1c10b69b7849e5620b61a13f4152b7149
Oliver Maketso <olivermaketso>
2024-10-01 12:45:14 +0300

    all work

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,9 +1,8 @@
 export default function getNeighborhoodsList() {
     this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
   
-    //using arrow function for addNeighborhood
     this.addNeighborhood = (newNeighborhood) => {
       this.sanFranciscoNeighborhoods.push(newNeighborhood);
-      return self.sanFranciscoNeighborhoods;
+      return this.sanFranciscoNeighborhoods;
     };
   }
