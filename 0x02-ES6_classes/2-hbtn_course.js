ab791d66e634342ed5c80cfaf3916524aec99160
Oliver Maketso <olivermaketso>
2024-10-04 15:12:46 +0300

    changes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,15 +1,5 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    if (typeof length !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
-    if (!Array.isArray(students) || !students.every((student) => typeof student === 'string')) {
-      throw new TypeError('students must be an array of strings');
-    }
-
     this._name = name;
     this._length = length;
     this._students = students;
@@ -22,6 +12,9 @@ export default class HolbertonCourse {
 
   // setter for name
   set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
     this._name = value;
   }
 
@@ -31,6 +24,9 @@ export default class HolbertonCourse {
   }
 
   set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
     this._length = value;
   }
 
@@ -40,6 +36,9 @@ export default class HolbertonCourse {
   }
 
   set students(value) {
+    if (!Array.isArray(value) || !value.every((student) => typeof student === 'string')) {
+      throw new TypeError('students must be an array of strings');
+    }
     this._students = value;
   }
 }
